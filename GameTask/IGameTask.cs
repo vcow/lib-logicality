4a369e12c1db30baa80837ced6608ea8bc9b006f
vcow <yvi@ambidexter.io>
2019-09-20 18:33:22 +0300

    logicality pack

---
+++ GameTask/IGameTask.cs
@@ -0,0 +1,25 @@
+﻿namespace Base.GameTask
+{
+	public delegate void GameTaskCompleteHandler(IGameTask task);
+
+	/// <summary>
+	/// Задача.
+	/// </summary>
+	public interface IGameTask
+	{
+		/// <summary>
+		/// Запуск задачи на исполнение.
+		/// </summary>
+		void Start();
+
+		/// <summary>
+		/// Флаг завершения задачи.
+		/// </summary>
+		bool Completed { get; }
+
+		/// <summary>
+		/// Событие завершения задачи.
+		/// </summary>
+		event GameTaskCompleteHandler CompleteEvent;
+	}
+}
