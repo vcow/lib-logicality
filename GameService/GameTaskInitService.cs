0be740a9b68449901545a163fa8d332d0f544b08
vcow <yvi@ambidexter.io>
2019-09-23 12:28:28 +0300

    game service added

---
+++ GameService/GameTaskInitService.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using Base.GameTask;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace Base.GameService
+{
+	/// <summary>
+	/// Вспомогательная задача на инициализацию сервиса.
+	/// </summary>
+	public class GameTaskInitService : IGameTask, IDisposable
+	{
+		private bool _completed;
+		private readonly IGameService _gameService;
+
+		private bool _isDisposed;
+		private bool _isStarted;
+
+		public GameTaskInitService(IGameService gameService)
+		{
+			_gameService = gameService;
+		}
+
+		// ITask
+
+		public bool Completed
+		{
+			get => _completed;
+			private set
+			{
+				if (value == _completed) return;
+
+				Assert.IsFalse(_completed);
+				_completed = value;
+				CompleteEvent?.Invoke(this);
+			}
+		}
+
+		public event GameTaskCompleteHandler CompleteEvent;
+
+		public void Start()
+		{
+			if (_isStarted || _isDisposed) return;
+			_isStarted = true;
+
+			if (_gameService.IsReady)
+			{
+				Debug.LogError("The Service is ready when the Task starts.");
+				Completed = true;
+				return;
+			}
+
+			_gameService.ReadyEvent += OnServiceReady;
+			_gameService.Initialize();
+		}
+
+		// \ITask
+
+		// IDisposable
+
+		public void Dispose()
+		{
+			if (_isDisposed) return;
+			_isDisposed = true;
+
+			_gameService.ReadyEvent -= OnServiceReady;
+		}
+
+		// \IDisposable
+
+		private void OnServiceReady(IGameService service)
+		{
+			_gameService.ReadyEvent -= OnServiceReady;
+			Completed = true;
+		}
+	}
+}
---
+++ GameService/IGameService.cs
@@ -0,0 +1,26 @@
+﻿namespace Base.GameService
+{
+	public delegate void GameServiceReadyHandler(IGameService service);
+
+	/// <summary>
+	/// Интерфейс сущности, требующей начальной инициализации
+	/// </summary>
+	public interface IGameService
+	{
+		/// <summary>
+		/// Начальная инициализация сервиса.
+		/// </summary>
+		/// <param name="args">Агрументы инициализации.</param>
+		void Initialize(params object[] args);
+
+		/// <summary>
+		/// Флаг готовности сервиса.
+		/// </summary>
+		bool IsReady { get; }
+
+		/// <summary>
+		/// Событие готовности сервиса.
+		/// </summary>
+		event GameServiceReadyHandler ReadyEvent;
+	}
+}
